e89e878b27f2a47377db49baee1356386bac5e91
Deekshant <deekshantsharma6@gmail.com>
2021-08-01 05:18:13 -0700

    everything done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
-import Todo from "./Comonents/Todo";
-function App() {
+import React, { useState,useEffect } from "react";
+import Header from "./Comonents/Header";
+import Form from "./Comonents/Form";
+import TodosList from "./Comonents/TodosList";
+import "./App.css";
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState(null);
+  
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // saving the todos in browser storage to prevent loss of todos on refreshing tab
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
   return (
-    <div>
-      <Todo />
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            edit={edit}
+            setEdit={setEdit}
+          />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} setEdit={setEdit} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
